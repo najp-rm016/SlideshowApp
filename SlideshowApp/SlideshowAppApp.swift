5b6db01cb28e6a565c38262eb271f7c311ae03fa
najp-rm016 <ryuichi.mochizuki@nippon-antenna.co.jp>
2024-01-22 23:52:06 -0800

    Initial Commit

---
+++ SlideshowApp/SlideshowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideshowAppApp.swift
+//  SlideshowApp
+//
+//  Created by NSPC201admin on 2024/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SlideshowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
